dbd03fa6fd2b941f47ed2466bb8bcfb29e92262a
Pewterschmidt <github@sushigang.online>
2024-02-26 22:11:25 +0100

    Correct github edit url in docusaurus.config.js"

--- docusaurus/RoboControl_WebSocket/docusaurus.config.js
+++ docusaurus/RoboControl_WebSocket/docusaurus.config.js
@@ -45,14 +45,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/Praktikant-Klobuerste/RoboControl_WebSocket/tree/dev/docusaurus/RoboControl_WebSocket",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/Praktikant-Klobuerste/RoboControl_WebSocket/tree/dev/docusaurus/RoboControl_WebSocket",
         },
         theme: {
           customCss: "./src/css/custom.css",
